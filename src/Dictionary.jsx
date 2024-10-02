00234001dd395c88eb7e685a7a02ef98953f076e
CoderRDX <rahuldev6258@gmail.com>
2024-10-02 14:04:42 +0530

    first commit

---
+++ src/Dictionary.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+const customDictionary = [
+
+    { word: "React", meaning: "A JavaScript library for building user interfaces." },
+
+    { word: "Component", meaning: "A reusable building block in React." },
+
+    { word: "State", meaning: "An object that stores data for a component." }
+
+];
+
+  export default function Dictionary(){
+
+    const [inputWord, setinputWord] = useState('');
+    const [meaning, setMeaning] = useState('');
+
+
+    const handleSearch = () => {
+        const result = customDictionary.find(entry => 
+            entry.word.toLowerCase() === inputWord.toLowerCase()
+          );
+          if (result) {
+            setMeaning(result.meaning);
+          } else {
+            setMeaning('Word not found in the dictionary');
+          }
+    };
+
+    return(
+        <div>
+        <h1>Dictionary App</h1>
+        <input 
+        type="text"
+        value={inputWord}
+        onChange={(e) => setinputWord(e.target.value.toLowerCase())}
+        placeholder="Search for a word..."
+         />
+         <button onClick={handleSearch}>Search</button>
+         <p><strong>Definition: </strong></p>
+          <p>{meaning}</p>
+        
+      </div>
+    );
+
+  }
